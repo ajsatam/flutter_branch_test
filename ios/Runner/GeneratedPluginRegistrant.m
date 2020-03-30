8ea093790352173fdf9c8527bb5e68f390530d82
Ajit Satam <ajcareful@gmail.com>
2020-03-30 20:55:59 +0530

    first code file commit.

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_branch_sdk/FlutterBranchSdkPlugin.h>)
+#import <flutter_branch_sdk/FlutterBranchSdkPlugin.h>
+#else
+@import flutter_branch_sdk;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterBranchSdkPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterBranchSdkPlugin"]];
+}
+
+@end
